abecad61b6784e7e248207c38d55e337d181c211
Gabs2000 <cesardz_2000_00@outlook.com>
2020-04-29 12:23:10 -0500

    Boton de prueba

--- Proyecto_OASIS/Proyecto_OASIS/Menu.Designer.cs
+++ Proyecto_OASIS/Proyecto_OASIS/Menu.Designer.cs
@@ -39,6 +39,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -149,6 +150,15 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(931, 307);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 109);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Boton de prueba";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 16F);
@@ -157,6 +167,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
@@ -188,6 +199,7 @@
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
