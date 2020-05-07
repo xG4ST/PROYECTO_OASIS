c817f4eef09c1194a1bb48d69fcd3ae7de57e6cc
Gabs2000 <cesardz_2000_00@outlook.com>
2020-05-07 18:31:30 -0500

    Conexion de Mysql

---
+++ Proyecto_OASIS/Proyecto_OASIS/MySql/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Proyecto_OASIS.MySql
+{
+    class Conexion
+    {
+        public static MySqlConnection obtainConexion()
+        {
+            MySqlConnection conexion = new MySqlConnection("server = 127.0.0.1; database = snack_db; Uid = root; pwd = ;");
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
