49a23d170a3982aad319fa5c4c9e0e263c031ce3
xG4ST <gus64_gast@outlook.es>
2020-05-12 12:07:30 -0500

    Programacion de Comida y Program

--- Proyecto_OASIS/Proyecto_OASIS/Comida.cs
+++ Proyecto_OASIS/Proyecto_OASIS/Comida.cs
@@ -7,17 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
 
 namespace Proyecto_OASIS
 {
     public partial class Comida : Form
     {
-        MySql.Comandos cma = new MySql.Comandos();
+        MySqlConnection conexion = new MySqlConnection("server = localhost; database=snack_db; Uid=root; pwd = ;");
+        
+        
+      
 
         public Comida()
         {
             InitializeComponent();
+            cargar_datos();
         }
+
+        public void cargar_datos()
+        {
+            conexion.Open();
+            MySqlCommand cm = new MySqlCommand"SELECT id_product,name_product FROM product", conexion);
+            //SELECT * FROM snack_db.product, name_product
+            //snack_db.product
+            //SELECT id_product,name_product FROM product
+            MySqlDataAdapter da = new MySqlDataAdapter(cm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conexion.Close();
+
+            DataRow fila = dt.NewRow(); ;
+            fila["name_product"] = "Seleccionar caomida";
+            dt.Rows.InsertAt(fila, 0);
+
+            comboBox1.ValueMember = "name_product";
+            comboBox1.DisplayMember = "id_product";
+            comboBox1.DataSource = dt;
+        }
+
+        public void carga_des_product(String name_product)
+        {
+            conexion.Open();
+            MySqlCommand cm = new MySqlCommand("SELECT id_product,name_product FROM product WHERE ", conexion);
+            cm.Parameters.AddWithValue("",);
+            MySqlDataAdapter da = new MySqlDataAdapter(cm);
+            DataTable d = new DataTable();
+            da.Fill(dt);
+
+
+            DataRow fila = dt.NewRow(); ;
+            dr["Aqui va lo que quieres seleccionar"] = "La seleccion";
+            dt.Rows.InsertAt(dr,0);
+
+            comboBox2.ValueMember = "";
+            comboBox2.DisplayMember = "No";
+            comboBox2.DataSource = dt;
+        }
+
 
         private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -41,12 +88,16 @@ namespace Proyecto_OASIS
 
         private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if(comboBox1.SelectedValue.ToString ()!= null)
+            {
+                String name_prod = comboBox1.SelectedValue.ToString();
+                cargar_datos(price_product)
+            }
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            cma.llenarCombo(comboBox1);
+         
         }
     }
 }
