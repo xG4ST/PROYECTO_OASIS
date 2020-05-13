9483e73914a85c5ecaf18aa27b700cd4044d773d
Gabs2000 <cesardz_2000_00@outlook.com>
2020-05-12 21:50:20 -0500

    Pantalla de comidas

--- Proyecto_OASIS/Proyecto_OASIS/Comida.Designer.cs
+++ Proyecto_OASIS/Proyecto_OASIS/Comida.Designer.cs
@@ -30,14 +30,11 @@
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
-            this.comboBox7 = new System.Windows.Forms.ComboBox();
-            this.comboBox8 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -45,9 +42,9 @@
             this.comboBox1.BackColor = System.Drawing.Color.DarkOrange;
             this.comboBox1.Font = new System.Drawing.Font("BigNoodleTitling", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 184);
+            this.comboBox1.Location = new System.Drawing.Point(52, 210);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(734, 78);
+            this.comboBox1.Size = new System.Drawing.Size(457, 78);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
@@ -56,31 +53,11 @@
             this.comboBox2.BackColor = System.Drawing.Color.DarkOrange;
             this.comboBox2.Font = new System.Drawing.Font("BigNoodleTitling", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(12, 307);
+            this.comboBox2.Location = new System.Drawing.Point(550, 210);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(734, 78);
             this.comboBox2.TabIndex = 1;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.BackColor = System.Drawing.Color.DarkOrange;
-            this.comboBox3.Font = new System.Drawing.Font("BigNoodleTitling", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(12, 453);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(734, 78);
-            this.comboBox3.TabIndex = 2;
-            // 
-            // comboBox4
-            // 
-            this.comboBox4.BackColor = System.Drawing.Color.DarkOrange;
-            this.comboBox4.Font = new System.Drawing.Font("BigNoodleTitling", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(12, 601);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(734, 78);
-            this.comboBox4.TabIndex = 3;
             // 
             // label2
             // 
@@ -93,46 +70,6 @@
             this.label2.Size = new System.Drawing.Size(269, 113);
             this.label2.TabIndex = 4;
             this.label2.Text = "CLIENTE";
-            // 
-            // comboBox5
-            // 
-            this.comboBox5.BackColor = System.Drawing.Color.DarkOrange;
-            this.comboBox5.Font = new System.Drawing.Font("BigNoodleTitling", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(788, 184);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(340, 78);
-            this.comboBox5.TabIndex = 6;
-            // 
-            // comboBox6
-            // 
-            this.comboBox6.BackColor = System.Drawing.Color.DarkOrange;
-            this.comboBox6.Font = new System.Drawing.Font("BigNoodleTitling", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(788, 307);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(340, 78);
-            this.comboBox6.TabIndex = 7;
-            // 
-            // comboBox7
-            // 
-            this.comboBox7.BackColor = System.Drawing.Color.DarkOrange;
-            this.comboBox7.Font = new System.Drawing.Font("BigNoodleTitling", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox7.FormattingEnabled = true;
-            this.comboBox7.Location = new System.Drawing.Point(788, 453);
-            this.comboBox7.Name = "comboBox7";
-            this.comboBox7.Size = new System.Drawing.Size(340, 78);
-            this.comboBox7.TabIndex = 8;
-            // 
-            // comboBox8
-            // 
-            this.comboBox8.BackColor = System.Drawing.Color.DarkOrange;
-            this.comboBox8.Font = new System.Drawing.Font("BigNoodleTitling", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox8.FormattingEnabled = true;
-            this.comboBox8.Location = new System.Drawing.Point(788, 601);
-            this.comboBox8.Name = "comboBox8";
-            this.comboBox8.Size = new System.Drawing.Size(340, 78);
-            this.comboBox8.TabIndex = 9;
             // 
             // button1
             // 
@@ -147,25 +84,42 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click_1);
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(52, 346);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(1474, 321);
+            this.dataGridView2.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Font = new System.Drawing.Font("BigNoodleTitling", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(0)))), ((int)(((byte)(109)))));
+            this.button2.Location = new System.Drawing.Point(1308, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 78);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Registrar";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // Comida
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Proyecto_OASIS.Properties.Resources.Fondo;
             this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox8);
-            this.Controls.Add(this.comboBox7);
-            this.Controls.Add(this.comboBox6);
-            this.Controls.Add(this.comboBox5);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox4);
-            this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Name = "Comida";
             this.Text = "Comida";
             this.Load += new System.EventHandler(this.Comida_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,13 +129,9 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox4;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.ComboBox comboBox6;
-        private System.Windows.Forms.ComboBox comboBox7;
-        private System.Windows.Forms.ComboBox comboBox8;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Proyecto_OASIS/Proyecto_OASIS/Comida.cs
+++ Proyecto_OASIS/Proyecto_OASIS/Comida.cs
@@ -13,19 +13,18 @@ namespace Proyecto_OASIS
         public Comida()
         {
             InitializeComponent();
-            cargar_datos();
         }
 
-        public void cargar_datos()
+        public void cargar_datos_alitas()
         {
             //SELECT* FROM snack_db.product, name_product
             //snack_db.product
             //SELECT id_product,name_product FROM product
 
-            string product = "Alita";
+            string product = "Alitas";
 
             conexion.Open();
-            MySqlCommand cm = new MySqlCommand("SELECT id_prod, name_prod FROM product WHERE name_prod = @product", conexion);
+            MySqlCommand cm = new MySqlCommand("SELECT id_prod, name_prod, des_prod FROM product WHERE name_prod = @product", conexion);
             cm.Parameters.AddWithValue("@product", product);
             MySqlDataAdapter da = new MySqlDataAdapter(cm);
             DataTable dt = new DataTable();
@@ -33,36 +32,97 @@ namespace Proyecto_OASIS
             conexion.Close();
 
             DataRow fila = dt.NewRow();
-            fila["name_prod"] = "Selecciona un producto";
+            fila["des_prod"] = "Seleccionar";
             dt.Rows.InsertAt(fila, 0);
 
-            comboBox1.ValueMember = "id_prod";
-            comboBox1.DisplayMember = "name_prod";
-            comboBox1.DataSource = dt;
+            comboBox2.ValueMember = "id_prod";
+            comboBox2.DisplayMember = "des_prod";
+            comboBox2.DataSource = dt;
         }
 
-        public void carga_des_product(String name_product)
+        public void cargar_datos_hamburguesa()
         {
-            //conexion.Open();
-            //MySqlCommand cm = new MySqlCommand("SELECT id_product, name_product, des_product FROM product WHERE name_product = Alita", conexion);
+            string product = "Hamburguesa";
 
+            conexion.Open();
+            MySqlCommand cm = new MySqlCommand("SELECT id_prod, name_prod, des_prod FROM product WHERE name_prod = @product", conexion);
+            cm.Parameters.AddWithValue("@product", product);
+            MySqlDataAdapter da = new MySqlDataAdapter(cm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conexion.Close();
 
+            DataRow fila = dt.NewRow();
+            fila["des_prod"] = "Seleccionar";
+            dt.Rows.InsertAt(fila, 0);
 
-            //cm.Parameters.AddWithValue("des_prod", );
-            //MySqlDataAdapter da = new MySqlDataAdapter(cm);
-            //DataTable cm = new DataTable();
-            //da.Fill(dt);
-            //return dt;
-
-            //DataRow fila = dt.NewRow(); ;
-            //dr["Aqui va lo que quieres seleccionar"] = "La seleccion";
-            //dt.Rows.InsertAt(dr, 0);
-
-            //comboBox2.ValueMember = "";
-            //comboBox2.DisplayMember = "No";
-            //comboBox2.DataSource = dt;
+            comboBox2.ValueMember = "id_prod";
+            comboBox2.DisplayMember = "des_prod";
+            comboBox2.DataSource = dt;
         }
 
+        public void cargar_datos_papas()
+        {
+            string product = "Papas";
+
+            conexion.Open();
+            MySqlCommand cm = new MySqlCommand("SELECT id_prod, name_prod, des_prod FROM product WHERE name_prod = @product", conexion);
+            cm.Parameters.AddWithValue("@product", product);
+            MySqlDataAdapter da = new MySqlDataAdapter(cm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conexion.Close();
+
+            DataRow fila = dt.NewRow();
+            fila["des_prod"] = "Seleccionar";
+            dt.Rows.InsertAt(fila, 0);
+
+            comboBox2.ValueMember = "id_prod";
+            comboBox2.DisplayMember = "des_prod";
+            comboBox2.DataSource = dt;
+        }
+
+        public void cargar_datos_bebida()
+        {
+            string product = "Bebida";
+
+            conexion.Open();
+            MySqlCommand cm = new MySqlCommand("SELECT id_prod, name_prod, des_prod FROM product WHERE name_prod = @product", conexion);
+            cm.Parameters.AddWithValue("@product", product);
+            MySqlDataAdapter da = new MySqlDataAdapter(cm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conexion.Close();
+
+            DataRow fila = dt.NewRow();
+            fila["des_prod"] = "Seleccionar";
+            dt.Rows.InsertAt(fila, 0);
+
+            comboBox2.ValueMember = "id_prod";
+            comboBox2.DisplayMember = "des_prod";
+            comboBox2.DataSource = dt;
+        }
+
+        public void cargar_datos_postres()
+        {
+            string product = "Postres";
+
+            conexion.Open();
+            MySqlCommand cm = new MySqlCommand("SELECT id_prod, name_prod, des_prod FROM product WHERE name_prod = @product", conexion);
+            cm.Parameters.AddWithValue("@product", product);
+            MySqlDataAdapter da = new MySqlDataAdapter(cm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conexion.Close();
+
+            DataRow fila = dt.NewRow();
+            fila["des_prod"] = "Seleccionar";
+            dt.Rows.InsertAt(fila, 0);
+
+            comboBox2.ValueMember = "id_prod";
+            comboBox2.DisplayMember = "des_prod";
+            comboBox2.DataSource = dt;
+        }
 
         private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -81,11 +141,55 @@ namespace Proyecto_OASIS
 
         private void Comida_Load(object sender, EventArgs e)
         {
-
+            comboBox1.Items.Add("Alitas");
+            comboBox1.Items.Add("Hamburguesa");
+            comboBox1.Items.Add("Papas");
+            comboBox1.Items.Add("Bebida");
+            comboBox1.Items.Add("Postres");
         }
 
         private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (comboBox1.SelectedItem.ToString() == "Alitas")
+            {
+                cargar_datos_alitas();
+            }
+            else
+            {
+            }
+
+            if (comboBox1.SelectedItem.ToString() == "Hamburguesa")
+            {
+                cargar_datos_hamburguesa();
+            }
+            else
+            {
+            }
+
+            if (comboBox1.SelectedItem.ToString() == "Papas")
+            {
+                cargar_datos_papas();
+            }
+            else
+            {
+            }
+
+            if (comboBox1.SelectedItem.ToString() == "Bebida")
+            {
+                cargar_datos_bebida();
+            }
+            else
+            {
+            }
+
+            if (comboBox1.SelectedItem.ToString() == "Postres")
+            {
+                cargar_datos_postres();
+            }
+            else
+            {
+            }
+
             // if (comboBox1.SelectedValue.ToString() != null)
             //{
             //  String name_prod = comboBox1.SelectedValue.ToString();
@@ -95,14 +199,12 @@ namespace Proyecto_OASIS
 
 
 
-
-
             //conexion.Open();
             //MySqlCommand cm = new MySqlCommand("SELECT id_product, name_product, des_product FROM product WHERE name_product = Alita");
             //MySqlDataAdapter adaptador = new MySqlDataAdapter(cm);
             //DataSet ds = new DataSet();
             //adaptador.Fill(ds);
-            
+
 
 
             //MySqlDataReader leer = cm.ExecuteReader();
@@ -135,6 +237,11 @@ namespace Proyecto_OASIS
             Confirmacion_de_pedido ToMenu = new Confirmacion_de_pedido();
             this.Hide();
             ToMenu.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
